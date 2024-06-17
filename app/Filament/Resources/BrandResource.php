6f492835b5391c7e42a78236d60cc92a17c63903
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-06-17 13:01:58 -0600

    Marcas: Modelo-Migracion-Recurso y traducciones

---
+++ app/Filament/Resources/BrandResource.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BrandResource\Pages;
+use App\Filament\Resources\BrandResource\RelationManagers;
+use App\Models\Brand;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BrandResource extends Resource
+{
+    protected static ?string $model = Brand::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-wallet';
+    protected static ?string $activeNavigationIcon = 'heroicon-s-shield-check';
+
+    public static function getNavigationLabel(): string
+    {
+        return __('Brands');
+    }
+
+
+    public static function getModelLabel(): string
+    {
+        return __('Brand');
+    }
+
+
+    public static function getPluralLabel(): ?string
+    {
+        return __('Brands');
+    }
+    public static function getNavigationGroup(): string
+    {
+        return __('Catalogs');
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->translateLabel()
+                    ->maxLength(100),
+                FileUpload::make('image')
+                    ->translateLabel()
+                    ->directory('brands')
+                    ->preserveFilenames(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->translateLabel()
+                    ->sortable()
+                    ->searchable(),
+                ImageColumn::make('image'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBrands::route('/'),
+            'create' => Pages\CreateBrand::route('/create'),
+            'edit' => Pages\EditBrand::route('/{record}/edit'),
+        ];
+    }
+}
